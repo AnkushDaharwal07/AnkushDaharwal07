1f4588e934c317f37aef075e06ed6bfa20fc595b
Ankush Daharwal <59690435+AnkushDaharwal07@users.noreply.github.com>
2023-09-01 09:19:18 +0530

    Update script.py

--- script.py
+++ script.py
@@ -12,10 +12,14 @@ def main():
     # Extract the names of functions with different isolatedClusters values
     different_function_names = []
     for func1, func2 in zip(yaml_data1['functions'], yaml_data2['functions']):
-        if func1.get('isolatedClusters') != func2.get('isolatedClusters'):
+        if (
+            func1.get('isolatedClusters') != func2.get('isolatedClusters') or
+            func1.get('colocatedClusters') != func2.get('colocatedClusters') or
+            func1.get('routingOptions') != func2.get('routingOptions')
+        ):
             different_function_names.append(func1['name'])
 
-    # Print the function names with different isolatedClusters values
+    # Print the function names with different isolatedClusters, colocatedClusters, or routingOptions values
     for name in different_function_names:
         print("Name:", name)
 
