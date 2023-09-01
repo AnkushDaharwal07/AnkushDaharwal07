111c3c5f8c1d3d69aef5a9db5fb792687e015720
Ankush Daharwal <59690435+AnkushDaharwal07@users.noreply.github.com>
2023-09-01 08:54:05 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,16 +4,8 @@
 json_data1=$(cat functions.yaml | yq eval -j)
 json_data2=$(cat functions.yaml | yq eval -j)
 
-# Extract the names of functions with changing isolatedClusters values
-previous_clusters=""
-echo "$json_data1" | jq -r '.functions[] | select(.isolatedClusters != $prev) | .name' --arg prev "$previous_clusters" | while read -r name; do
-  previous_clusters="$(echo "$json_data1" | jq -r --arg name "$name" '.functions[] | select(.name == $name) | .isolatedClusters')"
+IFS=$'\n' read -d '' -r -a names <<< "$(jq -r '.functions[] | select(.isolatedClusters != ($prev | fromjson)) | .name' --argjson prev "$json_data1" <<< "$json_data2"$'\x00')"
+
+for name in "${names[@]}"; do
   echo "Name: $name"
 done
-
-# Repeat for json_data2
-
-# Compare json_data1 and json_data2
-if [ "$json_data1" != "$json_data2" ]; then
-  echo "JSON data has changed between the two YAML files."
-fi
