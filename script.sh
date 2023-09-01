6bcbfb351e6690874fad77ab792aabd2b354fde4
Ankush Daharwal <59690435+AnkushDaharwal07@users.noreply.github.com>
2023-09-01 08:50:47 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,57 +1,19 @@
 #!/bin/bash
 
-# Define the JSON data
-json_data='
-{
-  "functions": [
-    {
-      "name": "CreateAcount.v1",
-      "isolatedClusters": [
-        "java-group2"
-      ]
-    },
-    {
-      "name": "CreateAcount.v2",
-      "isolatedClusters": [
-        "java-group3"
-      ]
-    },
-    {
-      "name": "CreateAcount.v3",
-      "colocatedClusters": [
-        "java-group12"
-      ],
-      "isolatedClusters": [
-        "java-group6"
-      ]
-    },
-    {
-      "name": "CreateAcount.v4",
-      "colocatedClusters": [
-        "java-group12"
-      ],
-      "isolatedClusters": [
-        "java-group6",
-        "java-group12"
-      ]
-    },
-    {
-      "name": "CreateAcount.v5",
-      "isolatedClusters": [
-        "java-group13"
-      ],
-      "routingOptions": {
-        "mitigatorEnabled": true
-      }
-    }
-  ]
-}
-'
+# Convert YAML files to JSON
+json_data1=$(cat functions.yaml | yq eval -j)
+json_data2=$(cat functions.yaml | yq eval -j)
 
 # Extract the names of functions with changing isolatedClusters values
 previous_clusters=""
-echo "$json_data" | jq -r '.functions[] | select(.isolatedClusters != $prev) | .name' --arg prev "$previous_clusters" | while read -r name; do
-  previous_clusters="$(echo "$json_data" | jq -r --arg name "$name" '.functions[] | select(.name == $name) | .isolatedClusters')"
+echo "$json_data1" | jq -r '.functions[] | select(.isolatedClusters != $prev) | .name' --arg prev "$previous_clusters" | while read -r name; do
+  previous_clusters="$(echo "$json_data1" | jq -r --arg name "$name" '.functions[] | select(.name == $name) | .isolatedClusters')"
   echo "Name: $name"
 done
 
+# Repeat for json_data2
+
+# Compare json_data1 and json_data2
+if [ "$json_data1" != "$json_data2" ]; then
+  echo "JSON data has changed between the two YAML files."
+fi
