319e1679aadd83096a5007e311e5a73b8a9f017d
Ankush Daharwal <59690435+AnkushDaharwal07@users.noreply.github.com>
2023-09-04 08:57:04 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
-# Define the function to load YAML data
-load_yaml() {
-    cat "$1" | python3 -c "import sys, yaml; print(yaml.safe_load(sys.stdin))"
-}
-
 # Load YAML data from the two files
-yaml_data1=$(load_yaml "functions.yaml")
-yaml_data2=$(load_yaml "functions2.yaml")
+yaml_data1=$(yq e '.functions' 'functions.yaml')
+yaml_data2=$(yq e '.functions' 'functions2.yaml')
 
 # Extract the names of functions with different isolatedClusters values
 different_function_names=()
-for ((i=0; i<${#yaml_data1[functions]}; i++)); do
-    func1=$(echo "$yaml_data1" | jq -r ".functions[$i]")
-    func2=$(echo "$yaml_data2" | jq -r ".functions[$i]")
-    if [ "$(echo "$func1" | jq -r .isolatedClusters)" != "$(echo "$func2" | jq -r .isolatedClusters)" ] || \
-       [ "$(echo "$func1" | jq -r .colocatedClusters)" != "$(echo "$func2" | jq -r .colocatedClusters)" ] || \
-       [ "$(echo "$func1" | jq -r .routingOptions)" != "$(echo "$func2" | jq -r .routingOptions)" ]; then
-        different_function_names+=("$(echo "$func1" | jq -r .name)")
+
+# Loop through the functions in the first file
+while IFS= read -r func1; do
+    # Get the name of the function
+    name=$(echo "$func1" | yq e '.name' -)
+
+    # Check if there is a function with the same name in the second file
+    func2=$(echo "$yaml_data2" | yq e "map(select(.name == \"$name\")) | .[0]" -)
+    if [[ -n "$func2" ]]; then
+        # Compare the isolatedClusters, colocatedClusters, and routingOptions values
+        if [[ "$(echo "$func1" | yq e '.isolatedClusters' -)" != "$(echo "$func2" | yq e '.isolatedClusters' -)" ]] ||
+           [[ "$(echo "$func1" | yq e '.colocatedClusters' -)" != "$(echo "$func2" | yq e '.colocatedClusters' -)" ]] ||
+           [[ "$(echo "$func1" | yq e '.routingOptions' -)" != "$(echo "$func2" | yq e '.routingOptions' -)" ]]; then
+            # Add the function name to the list of different function names
+            different_function_names+=("$name")
+        fi
     fi
-done
+done < <(echo "$yaml_data1")
 
 # Print the function names with different isolatedClusters, colocatedClusters, or routingOptions values
 for name in "${different_function_names[@]}"; do
