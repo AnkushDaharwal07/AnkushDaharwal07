d7f1d0b6ed8c08071925d7f5ab7d90e30c64a4ad
Ankush Daharwal <59690435+AnkushDaharwal07@users.noreply.github.com>
2023-09-04 08:38:03 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,32 +1,24 @@
 #!/bin/bash
 
-# Function to load YAML data from a file
-load_yaml() {
-    file_path="$1"
-    yq eval ' .' "$file_path"
+# Function to extract function names from a YAML file
+extract_function_names() {
+    grep 'name:' "$1" | awk '{print $2}'
 }
 
-# Load YAML data from the two files
-yaml_data1=$(load_yaml 'functions.yaml')
-yaml_data2=$(load_yaml 'functions2.yaml')
+# Function to compare two lists of function names
+compare_function_names() {
+    file1="$1"
+    file2="$2"
+    
+    functions1=($(extract_function_names "$file1"))
+    functions2=($(extract_function_names "$file2"))
 
-# Extract the names of functions with different isolatedClusters values
-different_function_names=()
-index=0
+    for func1 in "${functions1[@]}"; do
+        if [[ ! " ${functions2[@]} " =~ " $func1 " ]]; then
+            echo "Name: $func1"
+        fi
+    done
+}
 
-# Iterate through the function names in yaml_data1
-for name in $(echo "$yaml_data1" | jq -r '.functions[].name'); do
-    # Check if the same name exists in yaml_data2 with different properties
-    if ! echo "$yaml_data2" | jq -e --arg name "$name" \
-        '.functions[] | select(.name == $name and (.isolatedClusters != input.functions[] | select(.name == $name).isolatedClusters or
-        .colocatedClusters != input.functions[] | select(.name == $name).colocatedClusters or
-        .routingOptions != input.functions[] | select(.name == $name).routingOptions))' >/dev/null; then
-        different_function_names[$index]=$name
-        index=$((index+1))
-    fi
-done
-
-# Print the function names with different properties
-for name in "${different_function_names[@]}"; do
-    echo "Name: $name"
-done
+# Compare the two YAML files
+compare_function_names 'functions.yaml' 'functions2.yaml'
