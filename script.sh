201f8b6d57eb6affcb3d1330bde533095fe5d3e5
Ankush Daharwal <59690435+AnkushDaharwal07@users.noreply.github.com>
2023-09-04 08:44:19 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,38 +1,27 @@
+#!/bin/bash
 
-# Function to load YAML data from a file
+# Define the function to load YAML data
 load_yaml() {
-    file_path="$1"
-    yq eval ' .' "$file_path"
+    cat "$1" | python3 -c "import sys, yaml; print(yaml.safe_load(sys.stdin))"
 }
 
 # Load YAML data from the two files
-yaml_data1=$(load_yaml 'functions.yaml')
-yaml_data2=$(load_yaml 'functions2.yaml')
+yaml_data1=$(load_yaml "functions.yaml")
+yaml_data2=$(load_yaml "functions2.yaml")
 
 # Extract the names of functions with different isolatedClusters values
 different_function_names=()
-index=0
-
-for name1 in $(echo "$yaml_data1" | jq -r '.functions[].name'); do
-    name2=$(echo "$yaml_data2" | jq -r '.functions[] | select(.name == $name1).name')
-    if [ "$name1" != "$name2" ]; then
-        continue
-    fi
-
-    func1=$(echo "$yaml_data1" | jq -r '.functions[] | select(.name == $name1)')
-    func2=$(echo "$yaml_data2" | jq -r '.functions[] | select(.name == $name2)')
-
-    if [ \
-        "$(echo "$func1" | jq -r '.isolatedClusters')" != "$(echo "$func2" | jq -r '.isolatedClusters')" -o \
-        "$(echo "$func1" | jq -r '.colocatedClusters')" != "$(echo "$func2" | jq -r '.colocatedClusters')" -o \
-        "$(echo "$func1" | jq -r '.routingOptions')" != "$(echo "$func2" | jq -r '.routingOptions')" \
-    ]; then
-        different_function_names[$index]="$name1"
-        index=$((index+1))
+for i in $(seq 0 $((${#yaml_data1[functions][@]} - 1))); do
+    func1=$(echo "$yaml_data1" | jq -r ".functions[$i]")
+    func2=$(echo "$yaml_data2" | jq -r ".functions[$i]")
+    if [ "$(echo "$func1" | jq -r .isolatedClusters)" != "$(echo "$func2" | jq -r .isolatedClusters)" ] || \
+       [ "$(echo "$func1" | jq -r .colocatedClusters)" != "$(echo "$func2" | jq -r .colocatedClusters)" ] || \
+       [ "$(echo "$func1" | jq -r .routingOptions)" != "$(echo "$func2" | jq -r .routingOptions)" ]; then
+        different_function_names+=("$(echo "$func1" | jq -r .name)")
     fi
 done
 
-# Print the function names with different properties
+# Print the function names with different isolatedClusters, colocatedClusters, or routingOptions values
 for name in "${different_function_names[@]}"; do
     echo "Name: $name"
 done
