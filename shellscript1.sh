c2763963fd2fcb0fca09816235f00e419eef914e
Ankush Daharwal <59690435+AnkushDaharwal07@users.noreply.github.com>
2023-09-09 20:21:49 +0530

    Update shellscript1.sh

--- shellscript1.sh
+++ shellscript1.sh
@@ -6,20 +6,15 @@ git_diff_command="git diff $1 $2 -- functions.yaml"
 # Run the git diff command and capture the output
 diff_output=$($git_diff_command)
 
-# Print the git diff output for debugging
-echo "Git Diff Output:"
-echo "$diff_output"
+# Use 'grep' to extract lines starting with '- name:'
+name_lines=$(echo "$diff_output" | grep '^- name:')
 
-# Use 'grep' and 'awk' to extract -name values
-name_values=$(echo "$diff_output" | grep '^- name:' | awk '{print $3}')
-
-# Print the extracted name values for debugging
-echo "Extracted Name Values:"
-echo "$name_values"
-
-# Convert the extracted values to a list
-names_list=($name_values)
+# Extract -name values using 'cut' and store them in an array
+names_array=()
+while IFS= read -r line; do
+  name=$(echo "$line" | cut -d ' ' -f 3)
+  names_array+=("$name")
+done <<< "$name_lines"
 
 # Print the list of -name values
-echo "Final Names List:"
-echo "[${names_list[*]}]"
+echo "[${names_array[*]}]"
